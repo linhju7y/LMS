2c9ac2aa021f0601ae35a4267533e4643d823b12
linhju7y <linh.mhl9@gmail.com>
2021-06-29 01:48:50 +0700

    update

--- apiBase/options/branch.ts
+++ apiBase/options/branch.ts
@@ -3,7 +3,19 @@ import { instance } from "~/apiBase/instance";
 class BranchApi {
   getAll = () =>
     instance.get<IApiResult<IBranch[]>>("/api/Branch/GetAllBranch");
+
+  getPagination = (pageIndex: number) =>
+    instance.get<IApiResultData<IBranch[]>>("/api/Branch/GetAllBranch", {
+      params: {
+        page: pageIndex,
+      },
+    });
+
+  getDetail = (id: number) =>
+    instance.get<IApiResult<IBranch>>(`/api/Branch/GetBranch?id=${id}`);
+
   post = (data: IBranch) => instance.post("/api/Branch/InsertBranch", data);
+  put = (data: IBranch) => instance.put("/api/Branch/UpdateBranch", data, {});
 }
 
 export const branchApi = new BranchApi();
--- pages/option/provincial/index.tsx
+++ pages/option/provincial/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PowerTable from "~/components/PowerTable";
 import { data } from "../../../lib/option/dataOption";
 import FilterColumn from "~/components/Tables/FilterColumn";
@@ -7,24 +7,31 @@ import SortBox from "~/components/Elements/SortBox";
 import FilterTable from "~/components/Global/CourseList/FitlerTable";
 import ProvincialForm from "~/components/Global/Option/ProvincialForm";
 import LayoutBase from "~/components/LayoutBase";
+import { areaApi } from "~/apiBase";
+import { useWrap } from "~/context/wrap";
 
 const Provincial = () => {
   const columns = [
     {
-      title: "Provincial",
-      dataIndex: "provincial",
-      ...FilterColumn("provincial"),
+      title: "Mã tỉnh/thành phố",
+      dataIndex: "AreaCode",
+      ...FilterColumn("AreaCode"),
     },
-    { title: "Districts", dataIndex: "district", ...FilterColumn("district") },
+    {
+      title: "Tên tỉnh/thành phố",
+      dataIndex: "AreaName",
+      ...FilterColumn("AreaName"),
+    },
+    // { title: "Districts", dataIndex: "district", ...FilterColumn("district") },
     {
       title: "Modified By",
-      dataIndex: "rpCreator",
-      ...FilterColumn("rpCreator"),
+      dataIndex: "ModifiedBy",
+      ...FilterColumn("ModifiedBy"),
     },
     {
       title: "Modified Date",
-      dataIndex: "regDate",
-      ...FilterDateColumn("regDate"),
+      dataIndex: "ModifiedOn",
+      ...FilterDateColumn("ModifiedOn"),
     },
 
     {
@@ -36,16 +43,47 @@ const Provincial = () => {
     },
   ];
 
+  const [area, setArea] = useState<IArea[]>([]);
+  const [isLoading, setIsLoading] = useState({
+    type: "",
+    status: false,
+  });
+  const { showNoti } = useWrap();
+
+  const getDataArea = () => {
+    setIsLoading({
+      type: "GET_ALL",
+      status: true,
+    });
+    (async () => {
+      try {
+        let res = await areaApi.getAll();
+        res.status == 200 && setArea(res.data.createAcc);
+      } catch (error) {
+        showNoti("danger", error.message);
+      } finally {
+        setIsLoading({
+          type: "GET_ALL",
+          status: false,
+        });
+      }
+    })();
+  };
+
+  useEffect(() => {
+    getDataArea();
+  }, []);
+
   return (
     <PowerTable
       addClass="basic-header"
       TitlePage="Provincial List"
       TitleCard={<ProvincialForm showAdd={true} />}
-      dataSource={data}
+      dataSource={area}
       columns={columns}
+      loading={isLoading}
       Extra={
         <div className="extra-table">
-          <FilterTable />
           <SortBox />
         </div>
       }
